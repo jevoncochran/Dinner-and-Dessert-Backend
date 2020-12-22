69537a716876a041996b8989800bab257462e75c
Jevon Cochran <jevon.cochran@gmail.com>
2020-12-22 15:17:32 -0800

    update

--- migrations/20201202211710_menu_items.js
+++ migrations/20201202211710_menu_items.js
@@ -1,40 +1,25 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("menu_items", (tbl) => {
+    tbl.increments();
 
-exports.up = function(knex) {
-  return knex.schema
-    .createTable('menu_items', tbl => {
-        tbl.increments()
+    tbl.string("item", 100).notNullable();
 
-        tbl
-            .string('item', 100)
-            .notNullable()
+    tbl.string("description", 255);
 
-        tbl
-            .string('description', 255)
+    tbl.decimal("price", 2, 2).notNullable();
 
-        tbl
-            .decimal('price', 2, 2)
-            .notNullable()
+    tbl.string("dinner_or_dessert").notNullable();
 
-        tbl
-            .string('dinner_or_dessert')
-            .notNullable()
+    tbl.string("category");
 
-        tbl
-            .string('category')
+    tbl.string("image", 255);
 
-        tbl
-            .string('image', 255)
+    tbl.boolean("available_today");
 
-        tbl
-            .boolean('available_today')
-
-        tbl
-            .boolean('out')
-
-    })
+    tbl.boolean("out");
+  });
 };
 
-exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists('menu_items')
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("menu_items");
 };
--- migrations/20201202222656_orders.js
+++ migrations/20201202222656_orders.js
@@ -1,34 +1,27 @@
-exports.up = function(knex) {
-    return knex.schema
-      .createTable('orders', tbl => {
-          tbl.increments()
-  
-          tbl
-              .string('customer', 255)
-              .notNullable()
-  
-          tbl
-              .date('date')
-              .notNullable()
-  
-          tbl
-              .string('method')
-              .notNullable()
-  
-          tbl
-              .decimal('total', 2, 2)
-              .notNullable()
-  
-          tbl
-              .string('address', 255)
-  
-          tbl
-              .string('phone_number')
-              .notNullable()
-      })
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema
-      .dropTableIfExists('orders')
-  };
+exports.up = function (knex) {
+  return knex.schema.createTable("orders", (tbl) => {
+    tbl.increments();
+
+    tbl.string("customer", 255).notNullable();
+
+    tbl.date("date").notNullable();
+
+    tbl.string("method").notNullable();
+
+    tbl.decimal("subtotal", 2, 2).notNullable();
+
+    tbl.decimal("tip", 2, 2).notNullable();
+
+    tbl.decimal("tax", 2, 2).notNullable();
+
+    tbl.decimal("total", 2, 2).notNullable();
+
+    tbl.string("address", 255);
+
+    tbl.string("phone_number").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("orders");
+};
