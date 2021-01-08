bf5522d8555967d7e3e55a72fb3cd185d0b90ad5
Jevon Cochran <jevon.cochran@gmail.com>
2021-01-08 13:18:26 -0800

    added id argument to insert functions

--- inquiries/inquiries-model.js
+++ inquiries/inquiries-model.js
@@ -1,26 +1,24 @@
-const db = require('../data/dbConfig');
+const db = require("../data/dbConfig");
 
 module.exports = {
-    addInquiry,
-    getInquiries,
-    findInquiryById
-}
+  addInquiry,
+  getInquiries,
+  findInquiryById,
+};
 
 function addInquiry(inquiry) {
-    return db('inquiries')
-        .insert(inquiry)
-        .then(ids => {
-            const [id] = ids;
-            return findInquiryById(id);
-        })
+  return db("inquiries")
+    .insert(inquiry, "id")
+    .then((ids) => {
+      const [id] = ids;
+      return findInquiryById(id);
+    });
 }
 
 function getInquiries() {
-    return db('inquiries')
+  return db("inquiries");
 }
 
 function findInquiryById(id) {
-    return db('inquiries')
-        .where({ id })
-        .first()
+  return db("inquiries").where({ id }).first();
 }
